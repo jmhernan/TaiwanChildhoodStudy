5656973693fe8a263041e4d80db7a1cc7d5fb490
Jose Hernandez <jmhernandeze@gmail.com>
2021-09-17 14:25:15 -0700

    pytorch similarity function call for mean vector wip

--- torch_similarity.py
+++ torch_similarity.py
@@ -51,6 +51,84 @@ test_input = text[0:5]
 # tokenize text
 tokens = {'input_ids': [], 'attention_mask': []}
 
+###############
+# WIP create function to do this for each entry and output the single mean vector
+# per entry
+
+tokens_test = bert_tokenizer.encode_plus(text[0], max_length=128,
+                                            truncation=True, 
+                                            padding='max_length', 
+                                            return_tensors='pt')
+tokens_test['input_ids']
+tokens_test['attention_mask']
+
+outputs_test = bert_model(**tokens_test)
+outputs_test.keys()
+
+embeddings_test = outputs_test.last_hidden_state
+embeddings_test
+
+attention_mask_test = tokens_test['attention_mask']
+attention_mask_test.shape
+
+mask_test = attention_mask_test.unsqueeze(-1).expand(embeddings_test.size()).float()
+mask_test.shape
+mask_test
+
+masked_embeddings_test = embeddings_test * mask_test
+masked_embeddings_test.shape
+masked_embeddings_test
+
+# sum along one axis
+summed_test = torch.sum(masked_embeddings_test, 1)
+summed_test.shape
+
+# Then sum the number of values that must be given attention in each 
+# position of the tensor:
+summed_mask_test = torch.clamp(mask_test.sum(1), min=1e-9)
+summed_mask_test.shape
+
+# calculate mean pooled
+mean_pooled_test = summed_test / summed_mask_test
+
+# RETURN THIS
+# convert from PyTorch tensor to numpy array
+mean_pooled_test = mean_pooled_test.detach().numpy()
+###########
+
+# WIP function
+def embedding_mean_vector(txt, tokenizer, model):
+    # step 1 tokenize entry
+    tokens = tokenizer.encode_plus(txt, max_length=128,
+                                            truncation=True, 
+                                            padding='max_length', 
+                                            return_tensors='pt')
+    output = model(**tokens)
+    embeddings = output.last_hidden_state
+
+    attention_mask = tokens['attention_mask']
+    mask = attention_mask.unsqueeze(-1).expand(embeddings.size()).float()
+
+    masked_embeddings = embeddings * mask
+    # sum along one axis
+    summed = torch.sum(masked_embeddings, 1)
+    # Then sum the number of values that must be given attention in each 
+    # position of the tensor:
+    summed_mask = torch.clamp(mask.sum(1), min=1e-9)
+    # calculate mean pooled
+    mean_pooled = summed / summed_mask
+    # RETURN THIS
+    # convert from PyTorch tensor to numpy array
+    mean_pooled = mean_pooled.detach().numpy()
+    
+    return mean_pooled
+
+
+mean_pooled_2 = embedding_mean_vector(txt=text[0], tokenizer=bert_tokenizer, model=bert_model)
+mean_pooled_2.shape
+mean_pooled_test.shape
+###############
+
 for sentence in test_input:
     new_tokens = bert_tokenizer.encode_plus(sentence, max_length=128,
                                             truncation=True, 
